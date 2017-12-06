b13b0a4c4aa0909c049b15687fe44cb9865ae14e
spencermarchus <34144101+spencermarchus@users.noreply.github.com>
2017-12-06 15:56:11 -0600

    f
    f

---
+++ Patient/Patient.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Patient_Patient : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if((string)Session["type"] != "patient")
+        {
+            Label1.Text = "you aren't a patient";
+        }
+    }
+}
