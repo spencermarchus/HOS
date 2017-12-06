19b7e308f907793285980f73e77fbd51b1fadc62
spencermarchus <34144101+spencermarchus@users.noreply.github.com>
2017-12-06 01:41:12 -0600

    test

--- Account/Login.aspx.cs
+++ Account/Login.aspx.cs
@@ -4,37 +4,85 @@ using System;
 using System.Web;
 using System.Web.UI;
 using HOS;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
 
 public partial class Account_Login : Page
 {
-        protected void Page_Load(object sender, EventArgs e)
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        RegisterHyperLink.NavigateUrl = "Register";
+        OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
+        var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
+        if (!String.IsNullOrEmpty(returnUrl))
         {
-            RegisterHyperLink.NavigateUrl = "Register";
-            OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
-            var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
-            if (!String.IsNullOrEmpty(returnUrl))
+            RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
+        }
+    }
+
+    HOSDatabaseEntities dbcon = new HOSDatabaseEntities();
+
+    protected void LogIn(object sender, EventArgs e)
+    {
+        if (IsValid)
+        {
+            //clear cookies
+            Session.Clear();
+
+            // Validate the user password
+            var manager = new UserManager();
+            ApplicationUser user = manager.Find(UserName.Text, Password.Text);
+            if (user != null)
             {
-                RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
+                IdentityHelper.SignIn(manager, user, RememberMe.Checked);
+                IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+
+                string name = UserName.Text;
+
+                //determine if user is type doctor or patient
+                dbcon.PatientTables.Load();
+                string type = "patient";
+
+                //load list of patients & doctors
+                var patients =
+                    from patient in dbcon.PatientTables
+                    select patient;
+                //doctors
+                var doctors =
+                    from doctor in dbcon.DoctorTables
+                    select doctor;
+
+                //loop to check type
+                foreach (PatientTable p in patients)
+                {
+                    if (p.PatientUserName == name)
+                    {
+                        //do nothing, type is already equal to patient
+                        break; //exit loop
+                    }
+                }
+
+                foreach (DoctorTable d in doctors)
+                {
+                    if (d.DoctorUserName == name)
+                    {
+                        type = "doctor"; //user is doctor
+                        break; //exit loop
+                    }
+                }
+
+                Session.Add("name", name);
+                Session.Add("type", type);
+                string username = (string)Session[name];
             }
+            else
+            {
+                FailureText.Text = "Invalid username or password.";
+                ErrorMessage.Visible = true;
+            }
+
         }
 
-        protected void LogIn(object sender, EventArgs e)
-        {
-            if (IsValid)
-            {
-                // Validate the user password
-                var manager = new UserManager();
-                ApplicationUser user = manager.Find(UserName.Text, Password.Text);
-                if (user != null)
-                {
-                    IdentityHelper.SignIn(manager, user, RememberMe.Checked);
-                    IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-                }
-                else
-                {
-                    FailureText.Text = "Invalid username or password.";
-                    ErrorMessage.Visible = true;
-                }
-            }
-        }
+    }
 }
