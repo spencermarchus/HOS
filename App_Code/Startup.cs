2a63d582886bf0321edd81b781db42bf1ccc4d5d
spencermarchus <34144101+spencermarchus@users.noreply.github.com>
2017-12-06 01:37:41 -0600

    init

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HOS.Startup))]
+namespace HOS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
